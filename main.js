03a11943bc8212b2fc33931a70e948d22f8fe8fd
dorival42 <77635320+dorival42@users.noreply.github.com>
2021-02-18 12:39:08 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//selection de l'elemente
+const selectElement = function(element){
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click',function(){
+    body.classList.toggle('open');
+});
